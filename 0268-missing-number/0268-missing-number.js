90d5b64ecafaf0002ca69a4a5fece44cb8a18d2e
Mr Dantas <156125144+owesleydantas@users.noreply.github.com>
2025-04-25 19:57:01 -0300

    Time: 2 ms (53.56%), Space: 55.2 MB (74.61%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length;
+    for (let i = 0; i < nums.length; i++) {
+        n += i - nums[i];
+    }
+    return n;
+};

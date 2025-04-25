7109ce1943d4199ec8d6ad096ccb44e47704c7b9
Mr Dantas <156125144+owesleydantas@users.noreply.github.com>
2025-04-25 20:26:44 -0300

    Time: 2 ms (71.76%), Space: 60 MB (62.16%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+    let max = 0;
+
+    for (let value of nums) {
+        if (value === 1) {
+            count++
+            max = Math.max(max, count);
+        } else {
+            count = 0;
+        }
+    }
+    return max;
+};

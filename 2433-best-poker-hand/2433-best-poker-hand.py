59ea64cedeb7c703d6a8d6426c3939fb998c1a54
Mr Dantas <156125144+owesleydantas@users.noreply.github.com>
2025-05-01 17:14:37 -0300

    Time: 0 ms (100%), Space: 17.8 MB (61.19%) - LeetHub

---
+++ 2433-best-poker-hand/2433-best-poker-hand.py
@@ -0,0 +1,21 @@
+class Solution:
+    def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+        r = [0] * 14
+        n = len(suits)
+
+        for i in range(1, len(suits)):
+            if (suits[i - 1] != suits[i]):
+                break
+        else:
+            return "Flush"
+        
+        for rank in ranks:
+            r[rank] += 1
+        
+        res = max(r)
+        
+        if res >= 3:
+            return "Three of a Kind"
+        elif res == 2:
+            return "Pair"
+        return "High Card"

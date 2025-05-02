e21ace7bc7d72587b508068e4bcc7d0adc12d0e5
Mr Dantas <156125144+owesleydantas@users.noreply.github.com>
2025-05-02 15:55:57 -0300

    Time: 71 ms (99.34%), Space: 19.6 MB (81.91%) - LeetHub

---
+++ 0868-push-dominoes/0868-push-dominoes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        s = "L" + dominoes + "R"
+        prev, result, n = 0, list(s), len(s)
+
+        for i in range(1, n):
+            if s[i] == ".":
+                continue
+                
+            if i - prev > 1:
+                if s[prev] == s[i]:
+                    for k in range(prev + 1, i):
+                        result[k] = s[i]
+
+                elif s[prev] == "R" and s[i] == "L":
+                    l, r = prev + 1, i - 1
+                    while l < r:
+                        result[l] = "R"
+                        result[r] = "L"
+                        l += 1
+                        r -= 1
+            prev = i
+        
+        return "".join(result[1:-1])
